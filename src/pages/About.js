477ba3b912d20f809e476895e3ce50d1f1e064f5
alecrothenberg <alec.rothenberg@ufl.edu>
2022-06-08 20:12:41 -0400

    finished about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import alec_photo from "../assets/DSC_0386 - Copy - Copy.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              img src = {alec_photo}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Alec Rothenberg</div>
+            <div className="brief_description">
+              My name is Alec Rothenberg and I am a junior at the University of Florida. I am majoring in computer 
+              science with a minor in engineering innovation and a certificate in arificial intelligence. On campus, I am involved
+              with the engineering leadership council and lambda chi alpha. As well, I am a research assistant at the GLaDE
+              labarortory. In my free time, I like to go to the gym and cook.
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
